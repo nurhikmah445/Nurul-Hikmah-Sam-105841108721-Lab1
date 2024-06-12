c104d62a89609a7b943a4491ee10650f0b7c0d49
unknown <105841108721@student.unismuh.ac.id>
2024-06-12 16:37:41 +0700

    tugas 3 lab

--- App.js
+++ App.js
@@ -1,35 +1,80 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import React from "react"
 
 const App = () => {
   return (
-    <View style = {{ flex: 1 }}>
+    <View style={{flx: 1}}>
+     <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+
+     }}>
+      <Text style={{
+        fontSize: 50,
+        fontWeight: 'bold'
+
+      }}>Toko Oren</Text>
+      </View>
+
       <View style={{
-        flex: 1, alignItems: 'center',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+        
+      }}>
+        <Image source={require('./shopee.jpg')} />
+      </View>
+
+
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 100, 
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
 
       }}>
+
         <View style={{
-         backgroundColor: 'red' , width: 180, height: 100,
-         justifyContent: 'center',
-         borderRadius: 10,
+          backgroundColor: 'green',
+          width: 120,
+          height: 80,
+          borderRadius: 20,
+          justifyContent: 'center'
+
         }}>
-        <Text style={{
-          fontSize: 30, color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
-          Login
-        </Text>
-      </View>
-    </View>
-  </View>
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 30,
+            color: 'white',
+            fontWeight: 'bold'
+          }}>Login
+          </Text>
+          </View>
 
 
-  )
-      }   
+          <View style={{
+            backgroundColor: 'blue',
+            width: 120,
+            height: 80,
+            borderRadius: 20,
+            justifyContent: 'center'
 
-  
+          }}>
+            <Text style={{
+              textAlign: 'center',
+              fontSize: 30,
+              color: 'white',
+              fontWeight: 'bold'
+            }}>Sign Up
+            </Text>
+            </View>
 
-export default App
+          </View>
+
+          </View>
+
+      )
+    }
+    export default App

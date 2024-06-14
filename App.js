46bb5c348a7925145a99f643607293a436fa4ea8
unknown <105841108721@student.unismuh.ac.id>
2024-06-14 15:10:50 +0700

    Text Input

--- App.js
+++ App.js
@@ -1,80 +1,37 @@
-import { StyleSheet, View, Text, Image } from "react-native";
-import React from "react"
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+import React from 'react' ;
 
 const App = () => {
+const TextInputcustom = ({name, color }) => {
   return (
-    <View style={{flx: 1}}>
-     <View style={{
+      <TextInput
+      placeholder={`Mausukan ${name}`}
+      style={{
+      height: 40,
+      width: 200,
+      borderColor: 'tomato',
+      borderWidth: 1,
+      marginBottom: 10,
+      color: color,
+      padding: 10,
+      margin: 10,
+      borderRadius: 10,
+      paddingLeft: 10,
+    }}
+  />
+  )
+  }  
+
+  return (
+    <View style={{
       flex: 1,
+      justifyContent: 'center',
       alignItems: 'center',
-      justifyContent: 'center'
-
-     }}>
-      <Text style={{
-        fontSize: 50,
-        fontWeight: 'bold'
-
-      }}>Toko Oren</Text>
-      </View>
-
-      <View style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-        
-      }}>
-        <Image source={require('./shopee.jpg')} />
-      </View>
-
-
-      <View style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly'
-
-      }}>
-
-        <View style={{
-          backgroundColor: 'green',
-          width: 120,
-          height: 80,
-          borderRadius: 20,
-          justifyContent: 'center'
-
-        }}>
-          <Text style={{
-            textAlign: 'center',
-            fontSize: 30,
-            color: 'white',
-            fontWeight: 'bold'
-          }}>Login
-          </Text>
-          </View>
-
-
-          <View style={{
-            backgroundColor: 'blue',
-            width: 120,
-            height: 80,
-            borderRadius: 20,
-            justifyContent: 'center'
-
-          }}>
-            <Text style={{
-              textAlign: 'center',
-              fontSize: 30,
-              color: 'white',
-              fontWeight: 'bold'
-            }}>Sign Up
-            </Text>
-            </View>
-
-          </View>
-
-          </View>
-
-      )
-    }
-    export default App
+    }}>
+      <TextInputcustom name="Username" color="black">,</TextInputcustom>
+      <TextInputcustom name= "email" color="green"></TextInputcustom>
+      <TextInputcustom name="Password" color="red"></TextInputcustom>
+    </View>
+  )
+}
+  export default App

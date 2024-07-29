0bc3729f41f04b10e39d3ebc960ca70a472cf05d
nurhikmah445 <143022191+nurhikmah445@users.noreply.github.com>
2024-07-29 11:36:53 +0000

    Komponen Dasar

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
-import { StyleSheet, Text, View,TextInput } from 'react-native';
-import React from 'react' ;
+import { View, Text, Image } from 'react-native'
+import React from 'react'
 
 const App = () => {
-const TextInputcustom = ({name, color }) => {
   return (
-      <TextInput
-      placeholder={`Mausukan ${name}`}
-      style={{
-      height: 40,
-      width: 200,
-      borderColor: 'tomato',
-      borderWidth: 1,
-      marginBottom: 10,
-      color: color,
-      padding: 10,
-      margin: 10,
-      borderRadius: 10,
-      paddingLeft: 10,
-    }}
-  />
-  )
-  }  
-
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <TextInputcustom name="Username" color="black">,</TextInputcustom>
-      <TextInputcustom name= "email" color="green"></TextInputcustom>
-      <TextInputcustom name="Password" color="red"></TextInputcustom>
+    <View>
+      <Text>Hello Word</Text>
+      <Image source={require('./assets/img/shopee.jpg')} />
     </View>
   )
 }
-  export default App
+
+export default App

67529319e1b4c2226589b651f1681b48f08c3db9
nurhikmah445 <143022191+nurhikmah445@users.noreply.github.com>
2024-07-29 11:49:55 +0000

    View style dasar

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello Word</Text>
-      <Image source={require('./assets/img/shopee.jpg')} />
+    <View style={{flex:1}}>
+      <View style={{
+        flexDirection:'row',
+        flex:1, 
+        }}>
+
+      <View style={{
+        flex: 1,
+        backgroundColor:'red'
+      }}>
+      </View>
+      <View style={{
+        flex: 1,
+        backgroundColor:'blue'
+      }}>
+      </View>
+      <View style={{
+        flex: 1,
+        backgroundColor:'yellow'
+      }}>
+      </View>
+        </View>
+
+        <View style={{
+          flex: 1,
+          backgroundColor:'green'
+        }}>
+        </View>
+        <View style={{
+          flex: 1,
+          backgroundColor:'gray'
+        }}>
+        </View>
+
     </View>
   )
 }
